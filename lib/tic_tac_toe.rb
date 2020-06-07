cbf7f8e6913ecfe2ab2db0cb5aff12cdec437e06
Berkley Brown <berkleydeane@gmail.com>
2020-06-07 16:15:37 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,22 @@
+require = "pry"
+
+class TicTacToe 
+  
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    
+    
+    ]
+  
+def initialize(board = nil)
+  @board = board || Array.new(9, " ")
+  
+end  
+
+
+
+
+
+
+
+end  
