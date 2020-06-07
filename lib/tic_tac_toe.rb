cacc13e45b454e5800c4701c86265cf3b2fe8e9f
Berkley Brown <berkleydeane@gmail.com>
2020-06-07 20:55:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,7 +76,7 @@ end
 def won?
   
   WIN_COMBINATIONS.each do |win_combo|
-   @board[win_combo[0]] == "X" && @board[win_combo[1]] == "O" && 
+   @board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X"
   
   
      
