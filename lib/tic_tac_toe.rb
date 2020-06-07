ded6b73100874462199c16834899a74da2c2863d
Berkley Brown <berkleydeane@gmail.com>
2020-06-07 21:25:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -118,7 +118,15 @@ end
 
 
 def play
-  
+  @board = Array.new(9, " ")
+    # Play until someone wins or there is a draw
+    turn until over?
+    # Congratulate the winner
+    won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
+    # Ask if they'd like to play again
+    puts "Would you like to play again? (Y or N)"
+    # If yes, then #play again
+    gets.strip.downcase == "y" || gets.strip.downcase == "yes" ? play : puts("Goodbye!")
   
 end  
 
