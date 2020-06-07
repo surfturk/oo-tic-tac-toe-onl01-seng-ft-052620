759ff9100cd1d1b0231f2553ee63595be72ae12e
Berkley Brown <berkleydeane@gmail.com>
2020-06-07 17:53:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,7 +50,7 @@ end
 
 
 def turn
- puts "Player #{current_player}, please enter a number 1-9:"
+ puts "Player, please enter a number 1-9:"
     input = gets.strip
     index = input_to_index(input)
     cp = current_player
